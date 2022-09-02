b4c7321036cc00faa2c5c1bb4a6964449af676cf
Rafael Neto <rafaelneto.g@gmail.com>
2022-09-02 16:11:45 -0300

    FIRST PROGRAM CYCLE AND START REFACTOR TO CLASSES COMPONENTS

--- 5_reusable_code/build/index.js
+++ 5_reusable_code/build/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log('Hi there');
+Object.defineProperty(exports, "__esModule", { value: true });
+var manUnitedWins = 0;
+// for (const match of matches) {
+//   // SHOULD BE REFACTORED TO CLARIFY WHATS H AND A
+//   // WITH THE USE OF ENUMS
+//   if (match[1] === 'Man United' && match[5] === 'H') {
+//     manUnitedWins++;
+//   }
+//   if (match[2] === 'Man United' && match[5] === 'A') {
+//     manUnitedWins++;
+//   }
+// }
+// ENUM
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+// ENUNS USES A NORMAL OBJECT LIKE SYNTAX
+// THEY'RE USED TO INDICATE CLOSELY RELATED VALUES
+// const printMatchResult = (): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   }
+// };
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match_1 = matches_1[_i];
+    if (match_1[1] === 'Man United' && match_1[5] === 'H') {
+        manUnitedWins++;
+    }
+    if (match_1[2] === 'Man United' && match_1[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log("MAN UNITED WON " + manUnitedWins + " GAMES");
--- 5_reusable_code/src/index.ts
+++ 5_reusable_code/src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hi there')
+// this is a node standard funcion, so it should be using @types/node
+// to properly work
+import { match } from 'assert';
+import fs from 'fs';
+
+let manUnitedWins = 0;
+
+// for (const match of matches) {
+//   // SHOULD BE REFACTORED TO CLARIFY WHATS H AND A
+//   // WITH THE USE OF ENUMS
+//   if (match[1] === 'Man United' && match[5] === 'H') {
+//     manUnitedWins++;
+//   }
+//   if (match[2] === 'Man United' && match[5] === 'A') {
+//     manUnitedWins++;
+//   }
+// }
+
+// ENUM
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+// ENUNS USES A NORMAL OBJECT LIKE SYNTAX
+// THEY'RE USED TO INDICATE CLOSELY RELATED VALUES
+
+// const printMatchResult = (): MatchResult => {
+//   if (match[5] === 'H') {
+//     return MatchResult.HomeWin;
+//   }
+// };
+
+for (const match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  }
+  if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`MAN UNITED WON ${manUnitedWins} GAMES`);
